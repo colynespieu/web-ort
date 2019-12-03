c63bd97acadce24606659039294a8b8d7fc90411
Colyn <colyn@espieu.com>
2019-12-03 20:57:40 +0100

    Correction probleme injection SQL

--- TODOLIST_PHP_MYSQL/php/createtask.php
+++ TODOLIST_PHP_MYSQL/php/createtask.php
@@ -1,6 +1,7 @@
 <?php
 $title = $_POST['title'];
 $desc = $_POST['desc'];
+
 require("sqlConnector.php");
 $conn = new Sqlconnector();
 if ($title == "") {
@@ -8,8 +9,14 @@ if ($title == "") {
     echo "<meta http-equiv='refresh' content='3;URL=../index.php'>";
 }
 else {
-    $conn->addTask("$title","$desc");
-    echo "<p><strong>$title</strong> a été ajouté.</p>";
-    echo "<meta http-equiv='refresh' content='3;URL=../index.php'>";
+    if (substr_count($title,"\"") == 0 and substr_count($desc,"\"") == 0) {
+        $conn->addTask("$title","$desc");
+        echo "<p><strong>$title</strong> a été ajouté.</p>";
+        echo "<meta http-equiv='refresh' content='3;URL=../index.php'>";
+    }
+    else {
+        echo "<p>C'est mieux sans le <strong>\"</strong></p>";
+        echo "<meta http-equiv='refresh' content='3;URL=../index.php'>";
+    }
 }
 ?> 
